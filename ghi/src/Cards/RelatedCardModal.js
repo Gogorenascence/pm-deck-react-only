efcffa87e88a0f7b32f3ce5c968e70a31aea05b3
Kornebari <your@email.here>
2023-09-30 18:23:08 -0500

    RelatedCards Modal fixed

--- ghi/src/Cards/RelatedCardModal.js
+++ ghi/src/Cards/RelatedCardModal.js
@@ -35,7 +35,8 @@ function RelatedCardModal(props) {
                     onClick={handleShow}>
                     Show all Cards
             </button>
-
+            <Modal.Header closeButton>
+            </Modal.Header>
             <Modal
                 show={show}
                 size="xl"
@@ -46,20 +47,23 @@ function RelatedCardModal(props) {
                 <Modal.Body closeButton>
                 <h1 className="centered-h1"
                     style={{color: "black"}}>Related Cards</h1>
+                <div className="cd-inner2 card-pool-fill3">
+                    {relatedCards.map((relatedCard) => {
+                        return (
+                            <NavLink to={`/cards/${relatedCard.card_number}`}>
+                                    <img
+                                        className="cd-related-card"
+                                        title={relatedCard.name}
+                                        src={relatedCard.picture_url ? relatedCard.picture_url : "logo4p.png"}
+                                        alt={relatedCard.name}/>
+                            </NavLink>
+                        );
+                    })}
+                </div>
                 <div className="cd-inner">
-                    <div className="cd-inner card-pool-fill3">
-                        {relatedCards.map((relatedCard) => {
-                            return (
-                                <NavLink to={`/cards/${relatedCard.card_number}`}>
-                                        <img
-                                            className="cd-related-card"
-                                            title={relatedCard.name}
-                                            src={relatedCard.picture_url ? relatedCard.picture_url : "logo4p.png"}
-                                            alt={relatedCard.name}/>
-                                </NavLink>
-                            );
-                        })}
-                    </div>
+                    <button onClick={handleClose}>
+                        Close
+                    </button>
                 </div>
                 </Modal.Body>
             </Modal>
