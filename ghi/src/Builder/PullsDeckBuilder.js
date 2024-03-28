9013f68a0b67d0b10c187c2e1fe323385992ab2d
Kornebari <nantahkl@gmail.com>
2024-03-28 16:54:22 -0500

    Minor css change

--- ghi/src/Builder/PullsDeckBuilder.js
+++ ghi/src/Builder/PullsDeckBuilder.js
@@ -622,7 +622,7 @@ function PullsDeckBuilder(props) {
                                 <h2
                                     className="left"
                                     style={{margin: "1% 0px 1% 20px", fontWeight: "700"}}
-                                >Card Pool</h2>
+                                >Pulled Cards</h2>
                                 <img className="logo" src="https://i.imgur.com/YpdBflG.png" alt="cards icon"/>
                             </div>
                             <div className="inScrollable">
